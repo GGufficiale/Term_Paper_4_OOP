7f7b781e7426f3706f4044136e3dc3ba16317ab3
GGufficiale <germangranovskii@mail.ru>
2024-05-08 00:56:14 +0300

    Обновлены базовые классы

---
+++ src/class_ABS_actions_with_Vacancies_and_JSON_class.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+from src.class_Vacancies import Vacancy
+import json
+
+
+class ActionsWithVacancies(ABC):
+    """
+    Абстрактный Класс, который обязывает реализовать методы для добавления вакансий в файл, получения данных из файла
+    по критериям и удаления информации о вакансиях
+    """
+
+    @abstractmethod
+    def write_data(self, vacancies):
+        pass
+
+    @abstractmethod
+    def get_vacancies(self):
+        pass
+
+    @abstractmethod
+    def delete_vacancy(self, name):
+        pass
+
+
+class JsonSaver(ActionsWithVacancies):
+    def __init__(self, filename="vacancies.json"):
+        self.filename = f"data/{filename}"
+
+    def write_data(self, vacancies):
+        with open(self.filename, "w", encoding="utf-8") as f:
+            json.dump(vacancies, f, ensure_ascii=False, indent=4)
+
+    def get_vacancies(self) -> list[Vacancy]:
+        """Метод получения вакансий из json файла"""
+        with open(self.filename, encoding="utf-8") as f:
+            data = json.load(f)
+        vacancies = []
+        for vacancy in data:
+            """Распаковка значений словаря для создания экземпляров класса"""
+            vacancies.append(Vacancy(**vacancy))
+            # Можно было бы записать vacancies.append(Vacancy(name=vacancy["name"]) итд.)
+        return vacancies
+
+    def delete_vacancy(self, name) -> list[Vacancy]:
+        """Метод удаления вакансий из json файла"""
+        with open(self.filename, encoding="utf-8") as f:
+            data = json.load(f)
+            data.remove(name)
+        return data
